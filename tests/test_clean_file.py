81d37285bc88aaf59e854346295e69dc4b727421
Luiz Henrique dos Santos Vieira <89427184+Luizheennryy@users.noreply.github.com>
2025-06-06 20:56:14 -0300

    Update test_clean_file.py

--- tests/test_clean_file.py
+++ tests/test_clean_file.py
@@ -1,21 +1,15 @@
-# import os
-# from src.utils.csv_cleaner import clean_csv
+from src.utils.csv_cleaner import clean_file
 
-# def test_clean_file(tmp_path):
-#     pass
-    # # Cria um CSV temporário com conteúdo sujo
-    # dirty_content = "nome,idade\nJoão,25\nMaria,30\n'Alfredo',45"
-    # test_file = tmp_path / "dirty.csv"
-    # test_file.write_text(dirty_content, encoding="latin1")
+def test_clean_file(tmp_path):
+    dirty_content = "nome,idade\nJoão,25\nMaria,30\n'Alfredo',45"
+    test_file = tmp_path / "dirty.csv"
+    test_file.write_text(dirty_content, encoding="latin1")
 
-    # # Executa a limpeza
-    # clean_csv(str(test_file))
+    clean_file(str(test_file))
 
-    # # Lê novamente o arquivo limpo
-    # cleaned_content = test_file.read_text(encoding="latin1")
+    cleaned_content = test_file.read_text(encoding="latin1")
 
-    # # Verifica se a limpeza foi feita corretamente
-    # assert "'" not in cleaned_content
-    # assert "," not in cleaned_content
-    # assert ";" in cleaned_content
-    # assert "João;25" in cleaned_content
+    assert "'" not in cleaned_content
+    assert "," not in cleaned_content
+    assert ";" in cleaned_content
+    assert "João;25" in cleaned_content
